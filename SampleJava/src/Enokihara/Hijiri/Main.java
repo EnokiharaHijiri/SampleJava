32bc40dc32b6a610c3f2b03736a879ad250ac6bb
Hijiri Enokihara <sikounonazo@yahoo.co.jp>
2014-12-04 11:35:23 +0900

    First Commit

---
+++ SampleJava/src/Enokihara/Hijiri/Main.java
@@ -0,0 +1,13 @@
+package Enokihara.Hijiri;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Hello EGit!");
+	}
+
+}
